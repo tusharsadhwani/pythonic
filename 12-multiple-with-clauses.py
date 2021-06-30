21d94488246474eab8892967397de1a2b46db762
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-07-01 00:33:30 +0530

    Add snippet 12

---
+++ 12-multiple-with-clauses.py
@@ -0,0 +1,16 @@
+with open('test.html') as html, open('output.html', 'w') as out:
+    for index, line in enumerate(html):
+        try:
+            index = line.find('rgb(')
+            if index == -1:
+                continue
+
+            rgb_values = line[index+4:].rstrip(');\n')
+
+            red, green, blue = (int(num) for num in rgb_values.split(','))
+            hex_value = f'#{red:02x}{green:02x}{blue:02x}'
+            line = line[:index] + hex_value + ';\n'
+        except ValueError:
+            pass
+        finally:
+            out.write(line)
